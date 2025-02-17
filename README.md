5c2f2b078ecd377770a2ffb5318e76fab45c506f
tecwds <tecwds@163.com>
2025-02-17 13:49:29 +0800

    feature: starship nushell

---
+++ README.md
@@ -0,0 +1,30 @@
+# astro wezterm config
+
+> `wezterm` + `starship` + `nushell`
+
+# install
+
+1. ` wezterm`
+
+> TODO 添加环境变量配置
+
+```shell
+winget install wez.wezterm
+```
+
+2. `starship`
+
+> TODO 添加环境变量配置
+
+```shell
+winget install Starship.Starship
+```
+
+3. `nushell`
+
+> TODO 添加环境变量配置
+
+```shell
+winget install nushell
+```
+
--- settings.lua
+++ settings.lua
@@ -5,12 +5,21 @@ return function(config)
     -- 基础配置：去除 windows 原生窗口，使用 wezterm 自带窗口
     -- 注：鼠标显示比较慢，貌似有些 bug
     config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
+    -- 设置默认使用 NuShell
+    config.default_prog = { 'nu' }
 
     -- 设置窗口背景透明度
     config.window_background_opacity = 0.81
 
     -- 设置最大刷新率
     config.max_fps = 90
+
+    -- 使用 GPU 渲染
+    config.front_end = 'OpenGL'
+
+    -- 设置动画帧率
+    -- 注：需要指定 front_end 为 OpenGL，否则使用 CPU 渲染会导致卡顿，使用 CPU 渲染时 animation_fps 设置为 1 以此禁用动画
+    config.animation_fps = 90
 
 end
 
